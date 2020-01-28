00ea0672b167029267c980df63c66601e95e2041
Hung-Wei Tseng <htseng@ucr.edu>
2020-01-27 20:32:54 -0800

    Inital commit for the NPHeap Project

---
+++ kernel_module/src/ioctl.c
@@ -0,0 +1,105 @@
+//////////////////////////////////////////////////////////////////////
+//                             University of California, Riverside
+//
+//
+//
+//                             Copyright 2020
+//
+////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms and conditions of the GNU General Public License,
+// version 2, as published by the Free Software Foundation.
+//
+// This program is distributed in the hope it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+//
+////////////////////////////////////////////////////////////////////////
+//
+//   Author:  Hung-Wei Tseng
+//
+//   Description:
+//     Skeleton of NPHeap Pseudo Device
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "npheap.h"
+
+#include <asm/processor.h>
+#include <asm/segment.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/poll.h>
+#include <linux/mutex.h>
+
+extern struct miscdevice npheap_dev;
+
+int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+    return 0;
+}
+
+int npheap_init(void)
+{
+    int ret;
+    if ((ret = misc_register(&npheap_dev)))
+        printk(KERN_ERR "Unable to register \"npheap\" misc device\n");
+    else
+        printk(KERN_ERR "\"npheap\" misc device installed\n");
+    return ret;
+}
+
+void npheap_exit(void)
+{
+    misc_deregister(&npheap_dev);
+}
+
+
+// If exist, return the data.
+long npheap_lock(struct npheap_cmd __user *user_cmd)
+{
+    return 0;
+}     
+
+long npheap_unlock(struct npheap_cmd __user *user_cmd)
+{
+    return 0;
+}
+
+long npheap_getsize(struct npheap_cmd __user *user_cmd)
+{
+    return 0;
+}
+long npheap_delete(struct npheap_cmd __user *user_cmd)
+{
+    return 0;
+}
+
+long npheap_ioctl(struct file *filp, unsigned int cmd,
+                                unsigned long arg)
+{
+    switch (cmd) {
+    case NPHEAP_IOCTL_LOCK:
+        return npheap_lock((void __user *) arg);
+    case NPHEAP_IOCTL_UNLOCK:
+        return npheap_unlock((void __user *) arg);
+    case NPHEAP_IOCTL_GETSIZE:
+        return npheap_getsize((void __user *) arg);
+    case NPHEAP_IOCTL_DELETE:
+        return npheap_delete((void __user *) arg);
+    default:
+        return -ENOTTY;
+    }
+}
