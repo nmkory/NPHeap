2cc6796c9727cb3a35276e827ed44d4dd43ac563
Nick Kory <nmkory@ucdavis.edu>
2020-03-01 17:19:43 -0800

    Finished stubs and comments and info

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -45,10 +45,11 @@
 #include <linux/mutex.h>
 #include <linux/rbtree.h>
 
-
-/***********************************************************
-*   Global variables for NPHeap implementation.
-************************************************************/
+////////////////////////////////////////////////////////////////////////
+//
+//   Global variables for NPHeap implementation.
+//
+////////////////////////////////////////////////////////////////////////
 
 // Part of the starting code.
 extern struct miscdevice npheap_dev;
@@ -59,12 +60,13 @@ static DEFINE_MUTEX(np_lock);
 // The root node for the rb tree data structure.
 struct rb_root mytree = RB_ROOT;
 
-
-/***********************************************************
-*   Red black tree data structure implementation.
-*   Adapted from kernel.org documentation.
-*   https://www.kernel.org/doc/Documentation/rbtree.txt
-************************************************************/
+////////////////////////////////////////////////////////////////////////
+//
+//   Red black tree data structure implementation.
+//   Adapted from kernel.org documentation.
+//   https://www.kernel.org/doc/Documentation/rbtree.txt
+//
+////////////////////////////////////////////////////////////////////////
 
 // The rb tree node implementation.
 struct mytype {
@@ -72,9 +74,13 @@ struct mytype {
   	char *keystring;
   }; //struct mytype
 
-/*
- *
- */
+
+// my_search() searches through the rb tree for the node.
+//
+// rb_root: the rb tree rb_root
+// string: the data value we're searching for
+//
+// returns: the node we're looking for if found or null if not found
 struct mytype *my_search(struct rb_root *root, char *string)
 {
   	struct rb_node *node = root->rb_node;
@@ -95,14 +101,18 @@ struct mytype *my_search(struct rb_root *root, char *string)
 	return NULL;
 }  //my_search()
 
-/*
- *
- */
+
+// my_insert() inserts a node into the rb tree.
+//
+// rb_root: the rb tree rb_root
+// data: the node we're inserting
+//
+// returns: 1 if successful or 0 if the key value is already in the rb tree
 int my_insert(struct rb_root *root, struct mytype *data)
 {
   struct rb_node **new = &(root->rb_node), *parent = NULL;
 
-  /* Figure out where to put new node */
+  // Figure out where to put new node
   while (*new) {
     struct mytype *this = container_of(*new, struct mytype, node);
     int result = strcmp(data->keystring, this->keystring);
@@ -116,34 +126,76 @@ int my_insert(struct rb_root *root, struct mytype *data)
       return 0;
   }
 
-  /* Add new node and rebalance tree. */
+  // Add new node and rebalance tree.
   rb_link_node(&data->node, parent, new);
   rb_insert_color(&data->node, root);
 
 return 1;
 }  //my_insert()
 
-/*
- *
- */
+
+// rb_erase() is part of linux/rbtree.h.
+//
+// victim: node to be removed (found using search)
+// rb_root: the rb tree rb_root
+//
+// return: void
 //  void rb_erase(struct rb_node *victim, struct rb_root *tree);
+//
+// Example:
+//
+//   struct mytype *data = mysearch(mytree, "walrus");
+//
+//   if (data) {
+//   	rb_erase(data->node, mytree);
+//   	myfree(data);
+//   }
 
 
-/***********************************************************
-*   NPHeap implementation.
-************************************************************/
+////////////////////////////////////////////////////////////////////////
+//
+//   NPHeap implementation.
+//
+////////////////////////////////////////////////////////////////////////
 
-/*
- *
- */
+// This struct defines a memory VMM memory area. There is one of these
+// per VM-area/task.  A VM area is any part of the process virtual memory
+// space that has a special rule for the page-fault handlers (ie a shared
+// library, the executable area etc).
+//
+// struct vm_area_struct {
+//         struct mm_struct * vm_mm;       VM area parameters
+//         unsigned long vm_start;
+//         unsigned long vm_end;
+//         pgprot_t vm_page_prot;
+//         unsigned short vm_flags;
+// };
+
+// The npheap_cmd struct.
+//
+// struct npheap_cmd {
+//     __u64 op;	// 0 for lock, 1 for unlock
+//     __u64 offset;
+//     __u64 size;
+//     void *data;
+// };
+
+
+// npheap_mmap() creates a new mapping in the virtual address space of the
+// calling process.
+//
+// filp: unused
+// vma: the memory VMM memory area we are creating or mapping to
+//
+// returns: 0 if successful [TODO] maybe more
 int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+    unsigned long size = vma->vm_end - vma->vm_start;
     return 0;
 }  //npheap_mmap()
 
-/*
- *
- */
+
+// npheap_init() shouldn't be changed.
 int npheap_init(void)
 {
     int ret;
@@ -154,51 +206,61 @@ int npheap_init(void)
     return ret;
 }  //npheap_init()
 
-/*
- *
- */
+
+// npheap_exit() shouldn't be changed.
 void npheap_exit(void)
 {
     misc_deregister(&npheap_dev);
 }  //npheap_exit()
 
-/*
- *
- */
+
+// npheap_lock() aquires the mutex lock when available.
+//
+// user_cmd: unused
+//
+// returns: 0 when lock aquired
 long npheap_lock(struct npheap_cmd __user *user_cmd)
 {
   mutex_lock(&np_lock);
     return 0;
 }  //npheap_lock()
 
-/*
- *
- */
+
+// npheap_unlock() releases the mutex lock and wakes waiting users.
+//
+// user_cmd: unused
+//
+// returns: 0 when lock released
 long npheap_unlock(struct npheap_cmd __user *user_cmd)
 {
   mutex_unlock(&np_lock);
     return 0;
 }  //npheap_unlock()
 
-/*
- *
- */
+
+// npheap_getsize() returns the size of the user_cmd.
+//
+// user_cmd: the struct we need to find the size of in the rb tree
+//
+// returns: the size of the struct we're looking for or 0 if not found
 long npheap_getsize(struct npheap_cmd __user *user_cmd)
 {
     return 0;
 }  //npheap_getsize()
 
-/*
- *
- */
+
+// npheap_delete() deletes a node into the rb tree.
+//
+// user_cmd: the struct we need to find and delete/free
+//
+// returns: 0 if successful [TODO] maybe more
 long npheap_delete(struct npheap_cmd __user *user_cmd)
 {
     return 0;
 }  //npheap_delete()
 
-/*
- *
- */
+
+// npheap_ioctl() shouldn't be changed.
 long npheap_ioctl(struct file *filp, unsigned int cmd,
                                 unsigned long arg)
 {
